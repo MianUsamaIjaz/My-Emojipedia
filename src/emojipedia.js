df6f1a21a913151934495e75a604afb0b2d46e36
Mian Usama Ijaz <mianusama9090@gmail.com>
2023-07-21 20:52:44 -0230

    Initial Commit

---
+++ src/emojipedia.js
@@ -0,0 +1,25 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🥴",
+    name: "Face with unequal eyes and wavy mouth",
+    meaning:
+      "Eyes half open, half closed, slanted mouth, flushed cheeks. The emoji has a confused facial expression: due to tiredness or the currently confused emotional world. Also known as drunken face."
+  },
+  {
+    id: 2,
+    emoji: "😶",
+    name: "Face Without Mouth",
+    meaning:
+      "Emoji without a mouth. Is speechless, has no words. Someone does not want to or cannot say anything about a specific topic. Used in difficult, embarrassing or bad conversations. Imagine what it would say if it had a mouth!"
+  },
+  {
+    id: 3,
+    emoji: "🤥",
+    name: "Lying Face",
+    meaning:
+      "As with Pinocchio, the nose becomes longer with each lie, which exposes him as a liar. Symbol of a falsehood, boast, or an unlikely story."
+  }
+];
+
+export default emojipedia;
